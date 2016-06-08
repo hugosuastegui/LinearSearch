baba308b07adfe63bb67e45a63d3e37f7cc67c18
codeamac08 <codeamac08@gmail.com>
2016-06-08 10:03:33 -0500

    Linear and global linear search included

--- LinearSearch.rb
+++ LinearSearch.rb
@@ -1,4 +1,4 @@
-def linear_search(num, array)
+def linear_search(obj, array)
   if array.include?(num)
     for i in 0..array.length
       if  array[i] == num
@@ -10,8 +10,25 @@ def linear_search(num, array)
   end
 end
 
-random_numbers = [ 4, 3, 2, 20, 5, 64, 78, 11, 43 ]
-linear_search(20, random_numbers)
-# => 3
-linear_search(29, random_numbers)
-# => nil
+# random_numbers = [ 4, 3, 2, 20, 5, 64, 78, 11, 43 ]
+# linear_search(20, random_numbers)
+# # => 3
+# linear_search(29, random_numbers)
+# # => nil
+
+def global_linear_search(obj, array)
+  array_final = []
+  for i in 0..array.length
+    if  array[i] == obj
+      p i
+      array_final << i
+    end      
+  end
+  array_final
+end
+
+
+arr = "entretenerse".split('')
+
+p global_linear_search("e", arr)
+# => [0, 4, 6, 8, 11]
